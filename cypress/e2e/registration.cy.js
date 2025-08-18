7323549744a8050c1ac9289fa18813a84ee6b384
VeronikaVS90 <vsokolovanika@gmail.com>
2025-08-18 23:03:53 +0300

    completed tests for registration and login

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -7,6 +7,7 @@ describe('LoginHttp', () => {
     authorization.httpAuthorization();  
   });
 
+  // field Name
      it('Should show error for empty Name field', () => {
     cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
     cy.get('input#signupName').focus().blur();
@@ -19,6 +20,30 @@ describe('LoginHttp', () => {
     cy.contains('Name is invalid').should('be.visible');
   });
 
+  it('Should show error for invalid Name length', () => {
+  cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+  cy.get('input#signupName').clear().type('A').blur();
+  cy.contains('Name has to be from 2 to 20 characters long').should('exist');
+
+  const longName = 'A'.repeat(21); 
+  cy.get('input#signupName').clear().type(longName).blur();
+  cy.contains('Name has to be from 2 to 20 characters long').should('exist');
+
+  cy.get('input#signupName').clear().type('  A  ').blur();
+  cy.contains('Name is invalid').should('exist');
+  });
+  
+   it('Should have red border for invalid Name field', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupName').clear().type(' A ').blur();
+
+    cy.get('input#signupName')
+      .should('have.css', 'border-color')
+      .and('eq', 'rgb(220, 53, 69)'); 
+  });
+
   it('Should accept valid Name', () => {
     cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
     cy.get('input#signupName').type('Veronika').blur();
@@ -26,10 +51,77 @@ describe('LoginHttp', () => {
     cy.contains('Name is required').should('not.exist');
   });
 
+  // field Last name
+  it('Should show error for empty Last Name', () => {
+  cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+  cy.get('input#signupLastName').clear().blur();
+  cy.contains('Last name required').should('exist');
+  });
+  
+  it('Should show error for invalid Last Name (wrong data)', () => {
+  cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+  cy.get('input#signupLastName').clear().type('12345').blur();
+  cy.contains('Last name is invalid').should('exist');
+
+  cy.get('input#signupLastName').clear().type('@#$%').blur();
+  cy.contains('Last name is invalid').should('exist');
+  });
+  
+  it('Should show error for invalid Last Name length', () => {
+  cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+  cy.get('input#signupLastName').clear().type('B').blur();
+  cy.contains('Last name has to be from 2 to 20 characters long').should('exist');
+
+  const longLastName = 'B'.repeat(21);
+  cy.get('input#signupLastName').clear().type(longLastName).blur();
+  cy.contains('Last name has to be from 2 to 20 characters long').should('exist');
+
+  cy.get('input#signupLastName').clear().type('  B  ').blur();
+  cy.contains('Last name is invalid').should('exist');
+  });
+
+  it('Should have red border for invalid Last Name field', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupLastName').clear().type(' A ').blur();
+
+    cy.get('input#signupLastName')
+      .should('have.css', 'border-color')
+      .and('eq', 'rgb(220, 53, 69)'); 
+  });
+
+   it('Should accept valid Last name', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+    cy.get('input#signupName').type('Sokolova').blur();
+    cy.contains('Name is invalid').should('not.exist');
+    cy.contains('Name is required').should('not.exist');
+  });
+  
+// field "Email"
   it('Should show error for invalid Email', () => {
     cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
     cy.get('input#signupEmail').type('abc').blur();
     cy.contains('Email is incorrect').should('be.visible');
+  });
+
+   it('Should show error for empty Email', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupEmail').clear().blur();
+    cy.contains('Email required').should('exist');
+   });
+  
+  it('Should have red border for invalid Email field', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupEmail').clear().type('abc').blur();
+
+    cy.get('input#signupEmail')
+      .should('have.css', 'border-color')
+      .and('eq', 'rgb(220, 53, 69)'); 
   });
 
   it('Should accept valid Email', () => {
@@ -38,18 +130,96 @@ describe('LoginHttp', () => {
     cy.contains('Email is incorrect').should('not.exist');
   });
 
-  it('Should validate Password rules', () => {
+  // field Password
+   it('Should show error for empty Password', () => {
     cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
-    cy.get('input#signupPassword').type('short', { sensitive: true }).blur();
-    cy.contains('Password has to be from 8 to 15 characters long').should('be.visible');
+
+    cy.get('input#signupPassword').clear().blur();
+    cy.contains('Password required').should('exist');
   });
 
-  it('Should show error if passwords do not match', () => {
+  it('Should show error for invalid Password (wrong data)', () => {
     cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
-    cy.get('input#signupPassword').type('Nika54321', { sensitive: true });
-    cy.get('input#signupRepeatPassword').type('Nika54325', { sensitive: true }).blur();
-    cy.contains('Passwords do not match').should('be.visible');
+
+    cy.get('input#signupPassword').clear().type('Ab1').blur();
+    cy.contains('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
+      .should('exist');
+
+    cy.get('input#signupPassword').clear().type('password1').blur();
+    cy.contains('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
+      .should('exist');
+
+    cy.get('input#signupPassword').clear().type('PASSWORD1').blur();
+    cy.contains('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
+      .should('exist');
+
+    cy.get('input#signupPassword').clear().type('Password').blur();
+    cy.contains('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
+      .should('exist');
   });
+
+  it('Should have red border for invalid Password field', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupPassword').clear().type('123').blur();
+
+    cy.get('input#signupPassword')
+      .should('have.css', 'border-color')
+      .and('eq', 'rgb(220, 53, 69)'); 
+  });
+
+  it('Should accept valid Password', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+    cy.get('input#signupPassword').clear().type('Nika987654').blur();
+    cy.contains('Email is incorrect').should('not.exist');
+  });
+
+  // field Re-enter password
+  it('Should show error when Passwords do not match', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupPassword').clear().type('Password1');
+    cy.get('input#signupRepeatPassword').clear().type('Password2').blur();
+
+    cy.contains('Passwords do not match').should('exist');
+  });
+
+  it('Should show error for empty Re-enter Password field', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupRepeatPassword').clear().blur();
+    cy.contains('Re-enter password required').should('exist');
+  });
+
+  it('Should have red border for invalid Password field', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+
+    cy.get('input#signupPassword').clear().type('abc').blur();
+
+    cy.get('input#signupPassword')
+      .should('have.css', 'border-color')
+      .and('eq', 'rgb(220, 53, 69)'); 
+  });
+
+  it('Should accept valid Re-enter password', () => {
+    cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+     cy.get('input#signupPassword').clear().type('Nika987654');
+    cy.get('input#signupRepeatPassword').clear().type('Nika987654').blur();
+
+    cy.contains('Passwords do not match').should('not.exist');
+  });
+
+  // button Register
+it('Should keep Register button disabled for invalid form', () => {
+  cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
+  cy.get('input#signupName').type('Veronika');
+  cy.get('input#signupLastName').type('Sokolova');
+  cy.get('input#signupEmail').type('wrongemail');
+  cy.get('input#signupPassword').type('short', { sensitive: true }); 
+  cy.get('input#signupRepeatPassword').type('different', { sensitive: true }); 
+
+  cy.get('button.btn.btn-primary').should('be.disabled');
+});
 
   it('Should enable Register button for valid form', () => {
     cy.get('button.hero-descriptor_btn.btn.btn-primary').click();
@@ -73,6 +243,7 @@ describe('LoginHttp', () => {
       .clickRegistrationButton();
   });
     
+  // Login
     it('Should login with valid credentials via UI', () => {
   cy.login('vsokolovanika+123@gmail.com', 'Nika5432112345');
 });
